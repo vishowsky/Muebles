3a9f089113726391e91bec2581d72a8531b8a816
vishowsky <77794669+vishowsky@users.noreply.github.com>
2022-11-23 00:48:20 -0300

    agregado de productos
    se agrego la descripcion del producto

--- tienda/urls.py
+++ tienda/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('categorias', views.categorias, name='categorias')
+    path('categorias', views.categorias, name='categorias'),
+    path('categorias/<str:slug>', views.categoriasview, name="categoriasview"),
+    path('categorias/<str:categoriaSlug>/<str:productoSlug>', views.productosview, name="productosview"),
 ]
 
--- tienda/views.py
+++ tienda/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib import messages
 from .models import *
 # Create your views here.
 
@@ -10,3 +11,25 @@ def categorias(request):
     context = {'categoria':categoria}
     return render(request, "tienda/categorias.html", context)
 
+def categoriasview(request, slug):
+    if(Categoria.objects.filter(slug=slug, estado=0)):
+        productos = Producto.objects.filter(categoria__slug=slug)
+        categoria= Categoria.objects.filter(slug=slug).first()
+        context = {'productos': productos, 'categoria':categoria}
+        return render(request, "tienda/productos/index.html", context)
+    else:
+        messages.warning(request, "categoria no encontrado")
+        return redirect('categorias')
+
+def productosview(request, categoriaSlug, productoSlug):
+    if(Categoria.objects.filter(slug=categoriaSlug, estado=0)):
+        if(Producto.objects.filter(slug=productoSlug, estado=0)):
+            productos = Producto.objects.filter(slug=productoSlug, estado=0).first
+            context = {'productos':productos}
+        else:
+            messages.warning(request, "producto no encontrado")
+            return redirect('categorias')
+    else:
+        messages.error(request, "Categoria no encontrada")
+        return redirect('categorias')
+    return render(request, "tienda/productos/view.html", context)
